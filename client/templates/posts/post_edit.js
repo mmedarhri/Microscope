52b468a9160274aafe2976c59b85126f8ca6cc64
MEDARHRI <m.medarhri@gmail.com>
2014-11-02 13:27:16 +0100

    Actually use the error reporting.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,23 +1,20 @@
 Template.postEdit.events({
-  'submit form': function(e) {
-    e.preventDefault();
-    
-    var currentPostId = this._id;
-    
-    var postProperties = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
-    }
-    
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
-      if (error) {
-        // display the error to the user
-        alert(error.reason);
-      } else {
-        Router.go('postPage', {_id: currentPostId});
-      }
-    });
-  },
+ 'submit form': function(e) {
+ e.preventDefault();
+ var currentPostId = this._id;
+ var postProperties = {
+ url: $(e.target).find('[name=url]').val(),
+ title: $(e.target).find('[name=title]').val()
+ }
+ Posts.update(currentPostId, {$set: postProperties}, function(error) {
+ if (error) {
+ // display the error to the user
+ throwError(error.reason);
+ } else {
+ Router.go('postPage', {_id: currentPostId});
+ }
+ });
+ },
   
   'click .delete': function(e) {
     e.preventDefault();
