2a03794c0bf12c9bb0cc129d230b19bd95656f62
MEDARHRI <m.medarhri@gmail.com>
2014-11-05 00:43:37 +0100

    nly sync notifications that are relevant to the user.

--- server/publications.js
+++ server/publications.js
@@ -3,5 +3,6 @@ Meteor.publish('posts', function() { return Posts.find();
 Meteor.publish('comments', function(postId) { check(postId, String);
   return Comments.find({postId: postId});
 });
-Meteor.publish('notifications', function() { return Notifications.find();
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
 });
