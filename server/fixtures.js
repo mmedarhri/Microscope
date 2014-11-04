c7f5ec58450597d1323f5adcd8d057b8d0296f1b
MEDARHRI <m.medarhri@gmail.com>
2014-11-05 00:59:54 +0100

    Added enough posts that pagination is necessary.

--- server/fixtures.js
+++ server/fixtures.js
@@ -36,22 +36,18 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 3 * 3600 * 1000),
     body: 'You sure can Tom!'
   });
-  
-  Posts.insert({
-    title: 'Meteor',
-    userId: tom._id,
-    author: tom.profile.name,
-    url: 'http://meteor.com',
-    submitted: new Date(now - 10 * 3600 * 1000),
-    commentsCount: 0
-  });
-  
+
   Posts.insert({
     title: 'The Meteor Book',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
-    submitted: new Date(now - 12 * 3600 * 1000),
-    commentsCount: 0
+    submitted: new Date(now - 12 * 3600 * 1000), commentsCount: 0
   });
+  for (var i = 0; i < 10; i++) { Posts.insert({
+    title: 'Test post #' + i,
+    author: sacha.profile.name,
+    userId: sacha._id,
+    url: 'http://google.com/?q=test-' + i, submitted: new Date(now - i * 3600 * 1000), commentsCount: 0
+  }); }
 }
