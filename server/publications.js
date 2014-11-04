041f06656c7d8a56c16e1f5727beb1b58acd2d48
MEDARHRI <m.medarhri@gmail.com>
2014-11-05 00:34:39 +0100

    Display notifications in the header

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,7 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function() { return Posts.find();
 });
-
-Meteor.publish('comments', function(postId) {
-  check(postId, String);
+Meteor.publish('comments', function(postId) { check(postId, String);
   return Comments.find({postId: postId});
+});
+Meteor.publish('notifications', function() { return Notifications.find();
 });
