8598048d7320f5d3d766b31c5d4ab54756d9b792
MEDARHRI <m.medarhri@gmail.com>
2014-11-08 15:14:01 +0100

    ￼Commit 14-2 Fade items in when they are drawn.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -19,17 +19,16 @@ Template.postItem.helpers({
     }
   },
   attributes: function() {
-    var post = _.extend({}, Positions.findOne({postId: this._id}), this);
-    var newPosition = post._rank * POST_HEIGHT;
+    var post = _.extend({}, Positions.findOne({postId: this._id}), this); var newPosition = post._rank * POST_HEIGHT;
     var attributes = {};
-    if (! _.isUndefined(post.position)) {
-      var offset = post.position - newPosition; attributes.style = "top: " + offset + "px"; if (offset === 0)
+    if (_.isUndefined(post.position)) { attributes.class = 'post invisible';
+    } else {
+      var delta = post.position - newPosition; attributes.style = "top: " + delta + "px"; if (delta === 0)
         attributes.class = "post animate" }
     Meteor.setTimeout(function() {
       Positions.upsert({postId: post._id}, {$set: {position: newPosition}})
     });
-    return attributes; }
-});
+    return attributes; }});
 
 Template.postItem.events({
   'click .upvotable': function(e) {
