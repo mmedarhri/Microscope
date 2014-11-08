af699a5fdb850b665d1ffed2a922df31fe86be91
MEDARHRI <m.medarhri@gmail.com>
2014-11-08 13:38:04 +0100

    Commit 13-6 Added active classes to the header.

---
+++ client/templates/includes/header.js
@@ -0,0 +1,11 @@
+/**
+ * Created by medarhri on 08/11/14.
+ */
+Template.header.helpers({
+    activeRouteClass: function(/* route names */) {
+        var args = Array.prototype.slice.call(arguments, 0); args.pop();
+        var active = _.any(args, function(name) {
+            return Router.current() && Router.current().route.getName() === name
+        });
+        return active && 'active'; }
+});
